91bb9180d1ecad307554bbeae983ea1995bb11c0
Chris Wilkinson <chris@chriswilkinson.info>
2017-02-17 11:29:39 +0000

    teardown file tweak

--- destroy.sh
+++ destroy.sh
@@ -7,7 +7,7 @@ environment='dev'
 user='chris'
 
 cd infrastructure/terraform
-terraform destroy -var "access_key=$access_key" -var "secret_key=$secret_key" -var "user=$user" -var "environment=$environment"
+terraform destroy -var "access_key=$access_key" -var "secret_key=$secret_key" -var "user=$user" -var "environment=$environment" -var "web_instance_ami="
 
 cd ../nginx
 terraform destroy -var "access_key=$access_key" -var "secret_key=$secret_key" -var "user=$user" -var "environment=$environment"
